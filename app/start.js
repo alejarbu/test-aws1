3cb050bcbf27e2fbcad82604ef884c2973cecac2
SURAMERICANA\alejarbu <aarroyaveb@sura.com.co>
2017-03-24 16:23:30 -0500

    Add the package.json file to install and startup file for windows.

--- app/start.js
+++ app/start.js
@@ -1,46 +1,52 @@
-//Lets require/import the HTTP module
+const PORT = 8080;
 var http = require('http');
-//Dispatcher initialize
 var httpdispatcher = require('httpdispatcher');
+var fs = require('fs')
+var pixel = fs.readFileSync(__dirname + '/pixel.gif');
 var dispatcher = new httpdispatcher();
-//For all your static (js/css/images/etc.) set the directory name (relative path).
 dispatcher.setStaticDirname(__dirname);
 dispatcher.setStatic('resources');
-//Filesystem access to get the pixel in response
-var fs = require('fs'),
-    pixel = fs.readFileSync(__dirname + '/pixel.gif')
-//Lets define a port we want to listen to
-const PORT = 8080;
-//Lets use our dispatcher
-function handleRequest(request, response) {
-    try {
-        console.log(request.url);
-        dispatcher.dispatch(request, response);
-    } catch (err) {
-        console.log(err);
-    }
+
+function j2str(jobj) {
+    var cache = [];
+    var o = JSON.stringify(jobj, function(key, value) {
+        if (typeof value === 'object' && value !== null) {
+            if (cache.indexOf(value) !== -1) {
+                // Circular reference found, discard key
+                return;
+            }
+            // Store value in our collection
+            cache.push(value);
+        }
+        return value;
+    });
+    cache = null;
+    return o;
 }
 dispatcher.onGet("/tr", function(req, res) {
-    console.log(JSON.stringify(req));
-    res.end(pixel, 'binary');
-    //res.writeHead(200, {'Content-Type': 'text/plain'});
-    //res.end('Page One');
-});
-dispatcher.onGet("/", function(req, res) {
     res.writeHead(200, {
-        'Content-Type': 'text/plain'
+        'Content-Type': 'image/gif'
     });
-    res.end('welcome');
+    console.log(j2str(req));
+    res.end(pixel, 'binary');
+    // res.writeHead(200, {
+    //     'Content-Type': 'application/json'
+    // });
+    // res.end(j2str(req));
 });
 //A sample POST request
 /*dispatcher.onPost("/post1", function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Got Post Data');
 });*/
-//Create a server
-var server = http.createServer(handleRequest);
-//Lets start our server
+var server = http.createServer(function handleRequest(request, response) {
+    try {
+        //console.log(request.url);
+        dispatcher.dispatch(request, response);
+    } catch (err) {
+        console.log(err);
+    }
+});
 server.listen(PORT, function() {
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+    //console.log("Server listening on: http://localhost:%s", PORT);
 });
